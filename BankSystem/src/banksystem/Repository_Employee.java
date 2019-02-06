761fd4e8e165bfd8c427b7052ed8935789243be3
Xin Gao <Xin.Gao@yh.nackademin.se>
2019-02-06 20:38:30 +0100

    Gransnit

--- BankSystem/src/banksystem/Repository_Employee.java
+++ BankSystem/src/banksystem/Repository_Employee.java
@@ -4,34 +4,193 @@ Java18-OOJ
  */
 package banksystem;
 
+
+import banksystem.model.Account;
+import banksystem.model.AccountHistory;
+import banksystem.model.Client;
+import banksystem.model.Employee;
+import banksystem.model.HandleAccount;
+import banksystem.model.Loan;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 /**
  *
  * @author xingao
  */
 public class Repository_Employee {
-     public List<Client> getAllClients(){
-         List<Client> clients = new ArrayList<>();
-         return clients;
-     }
-     
-     public List<Account> getAllAccounts(){
-         List<Account> accounts = new ArrayList<>();
-         return accounts;
-     }
-     
     
-     public List<Loan> getAllLoan(){
-         List<Loan> loans = new ArrayList<>();
-         return loans;
-     }
+    private Properties p= new Properties();
+    
+    public Repository_Employee(){
+        try {
+            p.load(new FileInputStream("src/bankomat/Settings.properties"));
+//            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Client> getAllClients(){
+        Client client = new Client();
+        List<Client> clients = new ArrayList();
+        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), 
+            p.getProperty("name"), p.getProperty("password"));
+            Statement stmt = con.createStatement();){
+            
+            ResultSet rs = stmt.executeQuery("select idKund, personnummer, "
+                    + "pin, namn, address, telefon from Kund");
+            
+            while(rs.next()){
+                client = new Client(rs.getInt("idKund"), rs.getInt("personnummer"), rs.getInt("pin"),
+                        rs.getString("namn"), rs.getString("address"), rs.getString("telefon"));
+                clients.add(client);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        client.setClients(clients);
+        return clients;
+    }
+    
+    
+    public List<Account> getAllAccounts(){
+        Account account = new Account();
+        List<Account> accounts = new ArrayList();
+        boolean avsluta;
+        
+        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), 
+            p.getProperty("name"), p.getProperty("password"));
+            Statement stmt = con.createStatement();){
+            
+            ResultSet rs = stmt.executeQuery("select idKonto, number, "
+                    + "kundId, saldo, sparaRantesats, avsluta from Konto");
+            
+            while(rs.next()){
+                if (rs.getInt("avsluta") == 1){
+                    avsluta = true;
+                } else
+                    avsluta = false;
+                
+                account = new Account(rs.getInt("idKonto"), rs.getInt("kundId"), rs.getInt("number"),rs.getInt("saldo"), 
+                        rs.getDouble("sparaRantesats"), avsluta);
+                accounts.add(account);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        account.setAccounts(accounts);
+        return accounts;
+    }
+    
+    
+    public List<Loan> getAllLoans(){
+        Loan loan = new Loan();
+        List<Loan> loans = new ArrayList();
+        boolean bevilja;
+        //
+        
+        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), 
+            p.getProperty("name"), p.getProperty("password"));
+            Statement stmt = con.createStatement();){
+            
+            ResultSet rs = stmt.executeQuery("select idLan, LanNumber, "
+                    + "kundId, lanAntal, lanRantesats, betalplan, bevilja from Lan");
+            
+            while(rs.next()){
+
+                if (rs.getInt("bevilja") == 1){
+                    bevilja = true;
+                } else
+                    bevilja = false;
+                
+                loan = new Loan(rs.getInt("idLan"), rs.getInt("LanNumber"),
+                        rs.getInt("kundId"), rs.getInt("lanAntal"), 
+                        rs.getDouble("lanRantesats"), rs.getDate("betalplan"), bevilja);
+                loans.add(loan);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        loan.setLoans(loans);
+        return loans;
+    }
+    
+    /*
+    CREATE TABLE hanteraKonto (
+idhantering int(11) NOT NULL AUTO_INCREMENT,
+kontoId int(11) NOT NULL,
+sattainsaldo int(11) DEFAULT NULL,
+tautsaldo int(11) DEFAULT NULL,
+rantesats decimal(3,1) DEFAULT NULL,
+skapa tinyint(4) DEFAULT NULL,
+avsluta tinyint(4) DEFAULT NULL,
+anstalldId int(11) DEFAULT NULL,
+kundId int(11) DEFAULT NULL,
+date datetime NOT NULL,
+    */
+    
+    public List<HandleAccount> getAllHandleAccounts(){
+        HandleAccount handleAccount = new HandleAccount();
+        List<HandleAccount> historyOfAccounts = new ArrayList();
+        boolean avsluta;
+        boolean skapa;
+        
+        try(Connection con = DriverManager.getConnection(p.getProperty("connectionString"), 
+            p.getProperty("name"), p.getProperty("password"));
+            Statement stmt = con.createStatement();){
+            
+            ResultSet rs = stmt.executeQuery("select idhantering, kontoId, "
+                    + "sattainsaldo, tautsaldo, rantesats, skapa, avsluta, "
+                    + "anstalldId, kundId, date from hanteraKonto");
+            
+            while(rs.next()){
+                if (rs.getInt("avsluta") == 1){
+                    avsluta = true;
+                } else
+                    avsluta = false;
+
+                if (rs.getInt("skapa") == 1){
+                    skapa = true;
+                } else
+                    skapa = false;
+                
+                // (int id, int accountId, int depositAmount, int withdrawalAmount, 
+            // double rate, Date creationDate, boolean closedAccount,
+            // int employeeId, int clientId)
+                
+                handleAccount = new HandleAccount(rs.getInt("idhantering"), 
+                        rs.getInt("kontoId"), 
+                        rs.getInt("sattainsaldo"),
+                        rs.getInt("tautsaldo"),
+                        rs.getDouble("rantesats"),
+                        skapa,
+                        rs.getDate("date"),
+                        avsluta,
+                        rs.getInt("anstalldId"),
+                        rs.getInt("kundId"));
+                
+                historyOfAccounts.add(handleAccount);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        handleAccount.setHistoryOfAccounts(historyOfAccounts);
+        return historyOfAccounts;
+    }
+    
      
-     public List<AccountHistory> getAllHistory(){
-         List<AccountHistory> historys= new ArrayList<>();
-         return historys;
-     }
      
      public List<Employee> getAllEmployees(){
          List<Employee> employees= new ArrayList<>();
@@ -78,11 +237,11 @@ public class Repository_Employee {
          
      }
      
-     public double callPayOffMonth(int loanID){
-         
-     }
-     
-     public double callVinstOfLoan(int loanID){
-         
-     }
+//     public double callPayOffMonth(int loanID){
+//         
+//     }
+//     
+//     public double callVinstOfLoan(int loanID){
+//         
+//     }
 }
---
+++ BankSystem/src/banksystem/model/Employee.java
@@ -0,0 +1,47 @@
+/*
+ *  
+Java18-OOJ
+ */
+package banksystem.model;
+
+/**
+ *
+ * @author xingao
+ */
+public class Employee {
+    
+    private int id;
+    
+    private int number;
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+
+}
